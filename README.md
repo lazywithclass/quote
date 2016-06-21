164d197dae287d719195ce04fe910f2ed09a9c9b
lazywithclass <montecristoh@gmail.com>
2016-06-21 11:37:38 +0100

    basic instructions, added TODO as issue

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # quote
+
 Print a quote from the IMDB quotes database
+
+### Usage
+
+```bash
+# get the quotes from the FTP
+$ ./bin/fetch-quotes.sh
+# split the quotes into files (long! working on this)
+./bin/split.sh
+# print a random quote
+./bin/quote.sh
+```
--- bin/quote.sh
+++ bin/quote.sh
@@ -6,8 +6,3 @@ quotes=$(ls quotes-by-film/$randomfilm | wc -l | tr -d ' ')
 randomquote=$(gshuf -i 1-$quotes -n 1)
 
 cat quotes-by-film/$randomfilm/$randomquote
-
-
-# TODO
-# find a name, commit push and add these as issues
-# add a script that checks if we have to download a more recent version of the quotes

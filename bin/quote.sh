fe887ee5b1ad071a4a3f2332fabcaca5c26fa1f4
lazywithclass <montecristoh@gmail.com>
2016-06-22 22:16:55 +0100

    fixed paths to work outside the project

--- bin/quote.sh
+++ bin/quote.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-films=$(ls quotes-by-film | wc -l | tr -d ' ')
+script=$(realpath $0)
+path=$(dirname $script)
+
+films=$(ls $path/../quotes-by-film | wc -l | tr -d ' ')
 randomfilm=$(gshuf -i 1-$films -n 1)
-quotes=$(ls quotes-by-film/$randomfilm | wc -l | tr -d ' ')
+quotes=$(ls $path/../quotes-by-film/$randomfilm | wc -l | tr -d ' ')
 randomquote=$(gshuf -i 1-$quotes -n 1)
 
-cat quotes-by-film/$randomfilm/$randomquote
+cat $path/../quotes-by-film/$randomfilm/$randomquote

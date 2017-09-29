b8cdc9b8e4622c1898a45345d3f44fbc8a502a1f
lazywithclass <montecristoh@gmail.com>
2017-09-29 23:16:24 +0200

    url changed

--- bin/fetch-quotes.sh
+++ bin/fetch-quotes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget -O - ftp://ftp.fu-berlin.de/pub/misc/movies/database/quotes.list.gz | gunzip -c >quotes.list
+wget -O - ftp://ftp.fu-berlin.de/pub/misc/movies/database/temporaryaccess/quotes.list.gz | gunzip -c >quotes.list
